fd319f88857183985d01ff0e88ac551657c57d14
Jose Salazar <znx3p0@gmail.com>
2021-08-04 18:51:21 -0600

    secrets

--- tests/test.rs
+++ tests/test.rs
@@ -3,17 +3,18 @@ use async_std::task;
 use cirrus::prelude::*;
 use cirrus::providers::digital_ocean::*;
 use cirrus::providers::digital_ocean::response::{DropletSize, Images, Regions};
+use dotenv::dotenv;
 
-const KEY: &'static str = "8b23d874cc029210e3ee91dbfadf301e4025b8c9a3599ee87eecca60cbd28044";
 
 #[test]
 fn a() {
+    dotenv().unwrap();
     task::block_on(rm());
 }
 
 
 async fn rm() {
-    let d = DigitalOceanApi::new(KEY.into());
+    let d = DigitalOceanApi::new(std::env::var("KEY").unwrap());
     let request = Request::new("test".into(), Regions::Sfo3, DropletSize::Basic1C1G, Images::Ubuntu2004x64);
     let response = d.create_server(&request).await.unwrap();
     println!("{}", response.droplet.id);
